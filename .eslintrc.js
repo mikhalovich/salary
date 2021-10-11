8e9061cdb011a282b5c95bbb7f5b7616dc69c706
Ihnat Hantsevich <ignatgantsevich@gmail.com>
2021-10-11 10:28:10 +0300

    added axios, changed prettier configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,9 @@ module.exports = {
   parserOptions: {
     parser: 'babel-eslint',
   },
+  "plugins": [
+    "prettier"
+  ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
